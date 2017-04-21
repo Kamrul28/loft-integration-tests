cadb765dd7fa72a068bd8972805e6de0dd148a3d
kamrul28 <kamrul.nyc@gmail.com>
2017-04-20 21:32:15 -0700

    adding chrome capabilities

--- src/test/java/ui/common/TestHelper.java
+++ src/test/java/ui/common/TestHelper.java
@@ -4,7 +4,11 @@ import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -16,6 +20,17 @@ public class TestHelper {
 
     @Before
     public void setup() {
+        ChromeOptions options = new ChromeOptions();
+        Map<String, Object> pref = new HashMap<String, Object>();
+        pref.put("profile.default_content_settings.popups", 0);
+        pref.put("credentials_enable_service", false);
+        pref.put("password_manager_enabled", false);
+        options.addArguments("disable-extensions");
+        options.setExperimentalOption("prefs", pref);
+        options.addArguments("disable-infobars");
+        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+
         System.setProperty("webdriver.chrome.driver", "C:\\app\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
